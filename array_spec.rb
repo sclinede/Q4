8b9ed4acbd6e5037afae31111e8fe6dd1c016289
Sergey D <sclinede@gmail.com>
2013-06-04 15:23:24 +0600

    Add test for raise an NoMethodError

--- array_spec.rb
+++ array_spec.rb
@@ -1,9 +1,18 @@
 require './array'
 
 describe Array, "#to_s" do
+  let(:element_no_to_s_array) {  
+    o = Object.new
+    class << o; undef to_s; end
+    [o]
+  }
   let(:empty_array) { [] }
   let(:one_element_array) { [1] }
   let(:many_elements_array) { [1, 2, 3] }
+
+  it 'raise an NoMethodError if to_s not defined' do
+    expect { element_no_to_s_array.to_s }.to raise_error(NoMethodError)
+  end
 
   it 'returns "[]", when array is empty' do
     empty_array.to_s.should be == "[]"
